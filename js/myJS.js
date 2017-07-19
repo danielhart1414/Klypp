34053a6a6afbc92252a7cb022abaae48cc644518
Daniel Hart <daniel_hart@live.com>
2017-07-19 18:55:50 -0400

    added code to stop form from trying to submit

--- js/myJS.js
+++ js/myJS.js
@@ -1,4 +1,5 @@
 // Creates an alert to use for buttons when functionality is still in development
-$(".notReady").click(function(){
+$(".form").submit(function(event){
+    event.preventDefault();
     alert("This functionality is still in development.");
-})
+});
